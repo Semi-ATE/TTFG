e23e95392eb5ed7176ea4fd5615ef5ebc89e3162
Tom Hören <horen.tom@gmail.com>
2021-11-25 09:39:11 +0100

    Update test_ttg.py

--- tests/test_ttg.py
+++ tests/test_ttg.py
@@ -1,6 +1,6 @@
 # Copyright (c) Semi-ATE
 # Distributed under the terms of the MIT License
 
-# Tar archive To Github
+# Tar archive To Github (extract owners & permissions)
 def test_ttg():
     assert True
